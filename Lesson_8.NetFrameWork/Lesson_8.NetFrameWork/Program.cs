8ccf7391aff4ec96b64353f7d327cf22ec2ba5cc
FW960 <artuninp@gmail.com>
2022-02-25 02:40:35 +0800

    Создал Шаблон консольной программы в .NetFramework

---
+++ Lesson_8.NetFrameWork/Lesson_8.NetFrameWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8.NetFrameWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello world!";
+
+            int b = 33;
+
+            string result = $"{a} and b";
+
+            Console.WriteLine(result);
+        }
+    }
+}
